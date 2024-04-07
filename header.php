38282af78d819cdfb26f7d1bd767df068b7ed698
Madou Kone <madou.kone@grenoble-inp.org>
2024-04-07 16:48:33 +0200

    fitr end

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+    <div id="logo">
+        <a href="./index.html">
+            <img src="./logo3.jpg" alt="logo_team_ramadan">
+        </a>
+    </div>
+    <h1>Team Ramadan</h1>
+
+    <nav>
+        <a href="./index.html">ZAKAT</a>
+        <a href="./destin.html">La Nuit du destin</a>
+        <a href="./zakat_fitr.html">ZAKAT EL-FITR</a>
+        <a href="./days.html">Days</a>
+    </nav>
+</header>
